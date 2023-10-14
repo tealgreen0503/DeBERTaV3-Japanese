2bf9492570f81dd64cd20f5461387f32435a6869
KengoShimizu <tealgreen0503@gmail.com>
2023-10-14 16:18:09 +0900

    fix: remove __init__.py import

--- scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-from scripts.pre_tokenize import save_pre_tokenized_text
-from scripts.train_model import train_model
-from scripts.train_tokenizer import train_tokenizer
-
-__all__ = ["save_pre_tokenized_text", "train_tokenizer", "train_model"]
