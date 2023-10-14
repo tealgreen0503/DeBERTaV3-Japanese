3ba70f2cff11560d025e6f0f783c5354ac39d0c4
KengoShimizu <tealgreen0503@gmail.com>
2023-10-14 16:19:21 +0900

    refactor: change data download method

--- src/data.py
+++ src/data.py
@@ -3,8 +3,7 @@ from collections.abc import Generator
 from typing import Literal
 
 import datasets
-import tensorflow_datasets as tfds
-from datasets import Dataset, DatasetDict
+from datasets import Dataset, DatasetDict, load_dataset
 
 
 def download_dataset(dataset_names: list[Literal["wiki40b", "cc100", "oscar"]], seed: int = 42) -> DatasetDict:
@@ -28,6 +27,8 @@ def download_dataset(dataset_names: list[Literal["wiki40b", "cc100", "oscar"]], 
 
 def download_wiki40b() -> DatasetDict:
     def tf_wiki40b_generator(split: str) -> Generator[str, None, None]:
+        import tensorflow_datasets as tfds
+
         tf_dataset = tfds.load("wiki40b/ja", split=split)
         for aritcle in tf_dataset.as_numpy_iterator():
             is_paragraph = False
@@ -56,18 +57,11 @@ def download_wiki40b() -> DatasetDict:
 
 
 def download_cc100(seed: int) -> DatasetDict:
-    def tf_cc100_generator() -> Generator[str, None, None]:
-        tf_dataset = tfds.load("huggingface:cc100/lang=ja")
-        for sample in tf_dataset.as_numpy_iterator():
-            for line in sample["text"].decode().split("\n"):
-                if len(line) > 0:
-                    yield {"text": line}
-
     if os.path.isdir("data/raw/cc100"):
         return datasets.load_from_disk("data/raw/cc100")
     else:
-        dataset = Dataset.from_generator(tf_cc100_generator)
-        dataset_dict: DatasetDict = DatasetDict()
+        dataset = load_dataset("cc100", lang="ja").remove_columns("id")
+        dataset_dict = DatasetDict()
         dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
         dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
             test_size=0.5, seed=seed
@@ -77,18 +71,11 @@ def download_cc100(seed: int) -> DatasetDict:
 
 
 def download_oscar(seed: int) -> DatasetDict:
-    def tf_oscar_generator() -> Generator[str, None, None]:
-        tf_dataset = tfds.load("oscar/ja")
-        for sample in tf_dataset.as_numpy_iterator():
-            for line in sample["text"].decode().split("\n"):
-                if len(line) > 0:
-                    yield {"text": line}
-
     if os.path.isdir("data/raw/oscar"):
         return datasets.load_from_disk("data/raw/oscar")
     else:
-        dataset = Dataset.from_generator(tf_oscar_generator)
-        dataset_dict: DatasetDict = DatasetDict()
+        dataset = load_dataset("oscar-corpus/OSCAR-2301", language="ja").remove_columns(["id", "meta"])
+        dataset_dict = DatasetDict()
         dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
         dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
             test_size=0.5, seed=seed
