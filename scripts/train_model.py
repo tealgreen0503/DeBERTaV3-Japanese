9df7d349066b68d5fd7bb54d773111d1989e2900
KengoShimizu <tealgreen0503@gmail.com>
2023-10-22 12:45:18 +0900

    refactor: add __init__.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -17,7 +17,7 @@ from transformers import (
 )
 
 from src.data import download_dataset
-from src.models.modeling_deberta_v3 import DebertaV3ForPreTraining
+from src.models import DebertaV3ForPreTraining
 
 # Use save_to_disk() and load_from_disk() instead of using the cache
 datasets.disable_caching()
---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+from src.models.modeling_deberta_v3 import DebertaV3ForPreTraining, DebertaV3ForReplacedTokenDetection
+
+__all__ = ["DebertaV3ForPreTraining", "DebertaV3ForReplacedTokenDetection"]
