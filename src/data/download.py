6a3d91216f7632eec13b4e7e215f8e739b71e171
KengoShimizu <tealgreen0503@gmail.com>
2023-10-23 12:02:06 +0900

    refactor: remove mc4

--- src/data/download.py
+++ src/data/download.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Literal
 
@@ -16,17 +17,16 @@ from src.data.filters import (
     remove_wikipedia_footnote,
 )
 
-# Use save_to_disk() and load_from_disk() instead of using the cache
-datasets.disable_caching()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def download_dataset(
-    dataset_names: list[Literal["wikipedia", "cc100", "oscar", "mc4"]],
-    seed: int = 42,
-    is_training_tokenizer: bool = False,
+    dataset_names: list[Literal["wikipedia", "cc100", "oscar"]], seed: int = 42, is_training_tokenizer: bool = False
 ) -> DatasetDict:
     if is_training_tokenizer:
         dataset_names = list(set(dataset_names))
+
     dataset_dicts: list[DatasetDict] = []
     for dataset_name in dataset_names:
         match dataset_name:
@@ -36,20 +36,20 @@ def download_dataset(
                 dataset_dicts.append(download_cc100(seed))
             case "oscar":
                 dataset_dicts.append(download_oscar(seed))
-            case "mc4":
-                dataset_dicts.append(download_mc4(seed))
             case _:
-                raise Exception(f"Unsupported dataset: {dataset_name}")
+                raise ValueError(f"Unsupported dataset: {dataset_name}")
+
     dataset_dict = DatasetDict()
     if is_training_tokenizer:
         sampled_datasets: list[Dataset] = []
-        for dataset in dataset_dicts:
-            # Sample 1GB of data from each dataset
-            sample_size = 1e9 / dataset["train"].size_in_bytes
+        for _dataset_dict in dataset_dicts:
+            # Sample 1GB of data from each train dataset
+            dataset = _dataset_dict["train"]
+            sample_size = 1e9 / dataset.size_in_bytes
             assert sample_size <= 1
-            sampled_dataset, _ = dataset.train_test_split(train_size=sample_size, shuffle=True, seed=seed)
+            sampled_dataset, _ = dataset.train_test_split(train_size=sample_size, shuffle=True, seed=seed).values()
             sampled_datasets.append(sampled_dataset)
-        dataset_dict["train"] = datasets.concatenate_datasets(sampled_datasets, split=datasets.Split("train"))
+        dataset_dict["train"] = datasets.concatenate_datasets(sampled_datasets, split=datasets.Split.TRAIN)
     else:
         for split in ["train", "validation", "test"]:
             dataset_dict[split] = datasets.concatenate_datasets(
@@ -62,19 +62,20 @@ def download_wikipedia(seed: int) -> DatasetDict:
     if os.path.isdir("data/filtered/wikipedia"):
         return datasets.load_from_disk("data/filtered/wikipedia")
     else:
-        dataset = load_dataset("wikipedia", language="ja", date="20231020", beam_runner="DirectRunner").remove_columns(
-            ["id", "url", "title"]
-        )
+        dataset = load_dataset(
+            "wikipedia", language="ja", date="20231020", beam_runner="DirectRunner", split=datasets.Split.TRAIN
+        ).remove_columns(["id", "url", "title"])
+        logger.info(f"Completed downloading {dataset.info.dataset_name}: size={dataset.size_in_bytes / 1e9:.2f}GB")
 
         dataset = dataset.filter(is_not_empty())
-        dataset = dataset.map(remove_wikipedia_footnote())
-        dataset = dataset.map(remove_empty_parenthesis())
+        dataset = dataset.map(remove_wikipedia_footnote(), batched=True)
+        dataset = dataset.map(remove_empty_parenthesis(), batched=True)
 
         dataset_dict = DatasetDict()
-        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
+        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed).values()
         dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
             test_size=0.5, seed=seed
-        )
+        ).values()
 
         dataset_dict.save_to_disk("data/filtered/wikipedia")
     return dataset_dict
@@ -84,19 +85,20 @@ def download_cc100(seed: int) -> DatasetDict:
     if os.path.isdir("data/filtered/cc100"):
         return datasets.load_from_disk("data/filtered/cc100")
     else:
-        dataset = load_dataset("cc100", lang="ja", streaming=True).remove_columns("id")
+        dataset = load_dataset("cc100", lang="ja", split=datasets.Split.TRAIN, streaming=True).remove_columns("id")
+        logger.info(f"Completed downloading {dataset.info.dataset_name}: size={dataset.size_in_bytes / 1e9:.2f}GB")
 
         dataset = dataset.filter(is_not_empty())
         dataset = dataset.filter(is_japanese())
         dataset = dataset.filter(is_not_ad_content())
         dataset = dataset.filter(is_good_compression_ratio())
-        dataset = dataset.map(extract_japanese_text())
+        dataset = dataset.map(extract_japanese_text(), batched=True)
 
         dataset_dict = DatasetDict()
-        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
+        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed).values()
         dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
             test_size=0.5, seed=seed
-        )
+        ).values()
 
         dataset_dict.save_to_disk("data/filtered/cc100")
     return dataset_dict
@@ -106,7 +108,10 @@ def download_oscar(seed: int) -> DatasetDict:
     if os.path.isdir("data/filtered/oscar"):
         return datasets.load_from_disk("data/filtered/oscar")
     else:
-        dataset = load_dataset("oscar-corpus/OSCAR-2301", language="ja", streaming=True).remove_columns("id")
+        dataset = load_dataset(
+            "oscar-corpus/OSCAR-2301", language="ja", split=datasets.Split.TRAIN, streaming=True
+        ).remove_columns("id")
+        logger.info(f"Completed downloading {dataset.info.dataset_name}: size={dataset.size_in_bytes / 1e9:.2f}GB")
 
         dataset = dataset.filter(is_not_empty())
         dataset = dataset.filter(is_japanese())
@@ -114,38 +119,14 @@ def download_oscar(seed: int) -> DatasetDict:
         dataset = dataset.filter(is_valid_domain("oscar"))
         dataset = dataset.filter(is_not_ad_content())
         dataset = dataset.filter(is_good_compression_ratio())
-        dataset = dataset.map(extract_japanese_text())
         dataset = dataset.remove_columns(["meta"])
+        dataset = dataset.map(extract_japanese_text(), batched=True)
 
         dataset_dict = DatasetDict()
-        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
+        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed).values()
         dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
             test_size=0.5, seed=seed
-        )
+        ).values()
 
         dataset_dict.save_to_disk("data/filtered/oscar")
-    return dataset_dict
-
-
-def download_mc4(seed: int) -> DatasetDict:
-    if os.path.isdir("data/filtered/mc4"):
-        return datasets.load_from_disk("data/filtered/mc4")
-    else:
-        dataset = load_dataset("mc4", languages=["ja"], streaming=True).remove_columns("timestamp")
-
-        dataset = dataset.filter(is_not_empty())
-        dataset = dataset.filter(is_japanese())
-        dataset = dataset.filter(is_valid_domain("mc4"))
-        dataset = dataset.filter(is_not_ad_content())
-        dataset = dataset.filter(is_good_compression_ratio())
-        dataset = dataset.map(extract_japanese_text())
-        dataset = dataset.remove_columns(["url"])
-
-        dataset_dict = DatasetDict()
-        dataset_dict["train"], valid_test_dataset = dataset.train_test_split(test_size=0.1, seed=seed)
-        dataset_dict["validation"], dataset_dict["test"] = valid_test_dataset.train_test_split(
-            test_size=0.5, seed=seed
-        )
-
-        dataset_dict.save_to_disk("data/filtered/mc4")
     return dataset_dict
