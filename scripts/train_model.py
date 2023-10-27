9fc4e680822c0334868cdc6445512118608902c7
KengoShimizu <tealgreen0503@gmail.com>
2023-10-28 01:03:25 +0900

    feat: add sentence boundary detection preprocess

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,17 +1,21 @@
 import argparse
+import bisect
 import os
 import shutil
 from pathlib import Path
 from typing import Any
 
 import datasets
+import pysbd
 import torch
 import yaml
 from dotenv import load_dotenv
 from transformers import (
+    BatchEncoding,
     DataCollatorForLanguageModeling,
     DebertaV2Config,
     DebertaV2TokenizerFast,
+    PreTrainedTokenizerFast,
     Trainer,
     TrainingArguments,
 )
@@ -20,21 +24,79 @@ from src.data import download_dataset
 from src.models import DebertaV3ForPreTraining
 
 
+def batch_preprocess(
+    batch_example: dict[str, list[Any]],
+    tokenizer: PreTrainedTokenizerFast,
+    text_segmenter: pysbd.Segmenter,
+    max_length: int = 512,
+) -> dict[str, list[Any]]:
+    batch_encoding = tokenizer.batch_encode_plus(
+        batch_example["text"], add_special_tokens=False, padding=False, truncation=False
+    )
+    batch_encoding = batch_segment_text_into_sentences(
+        batch_encoding, batch_example["text"], tokenizer, text_segmenter, max_length=max_length
+    )
+    return batch_encoding
+
+
+def batch_segment_text_into_sentences(
+    batch_encoding: BatchEncoding,
+    batch_text: list[str],
+    tokenizer: PreTrainedTokenizerFast,
+    text_segmenter: pysbd.Segmenter,
+    max_length: int = 512,
+) -> dict[str, list[Any]]:
+    batch_outputs: dict[str, list[Any]] = {}
+    for encoding, text in zip(batch_encoding.encodings, batch_text, strict=True):
+        end_char_index_candidates = [text_span.end for text_span in text_segmenter.segment(text)]
+
+        encoding_length = len(encoding.ids)
+        start_token_index = 0
+        while start_token_index < encoding_length:
+            end_token_index_limit = start_token_index + max_length - 2
+
+            if end_token_index_limit < encoding_length:
+                end_char_index_limit = encoding.token_to_chars(end_token_index_limit)[1]
+                end_char_index_candidate_index = bisect.bisect(end_char_index_candidates, end_char_index_limit) - 1
+                if end_char_index_candidate_index >= 0:
+                    end_char_index = end_char_index_candidates[end_char_index_candidate_index]
+                    end_token_index = encoding.char_to_token(end_char_index)
+                    end_char_index_candidates = end_char_index_candidates[end_char_index_candidate_index + 1 :]
+                else:
+                    end_token_index = end_token_index_limit
+            else:
+                end_token_index = encoding_length
+
+            ids = encoding.ids[start_token_index:end_token_index]
+            outputs = tokenizer.prepare_for_model(ids, add_special_tokens=True, padding=False, truncation=False)
+
+            for key, value in outputs.items():
+                if key not in batch_outputs:
+                    batch_outputs[key] = []
+                batch_outputs[key].append(value)
+
+            start_token_index = end_token_index
+
+    return BatchEncoding(batch_outputs)
+
+
 def train_model(config: dict[str, Any], local_rank: int = -1) -> None:
     load_dotenv()
     config_discriminator = DebertaV2Config(**config["model"]["discriminator"])
     config_generator = DebertaV2Config(**config["model"]["generator"])
 
     tokenizer = DebertaV2TokenizerFast.from_pretrained(Path("models") / config["model_name"])
+    text_segmenter = pysbd.Segmenter(language="ja", clean=False, char_span=True)
 
     if os.path.isdir("data/encoded"):
         dataset_dict = datasets.load_from_disk("data/encoded")
     else:
         dataset_dict = download_dataset(config["dataset_names"], seed=config["seed"])
         dataset_dict = dataset_dict.map(
-            lambda x: tokenizer(x["text"], truncation=True, return_overflowing_tokens=True),
+            batch_preprocess,
             batched=True,
             remove_columns="text",
+            fn_kwargs={"tokenizer": tokenizer, "text_segmenter": text_segmenter},
         )
         dataset_dict.save_to_disk("data/encoded")
 
